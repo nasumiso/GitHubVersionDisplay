9f2754c2684003f77928604ee6e14bfe14648df6
K.Sohara <srqys795@yahoo.co.jp>
2025-03-31 16:24:43 +0900

    テストプロジェクトを追加(動作未確認)
    Macローカルでの動作確認はまだできておりません。
    （XUnitの認識ができていない）

---
+++ GitHubVersionDisplay.Tests/VersionTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using System.Reflection;
+
+namespace GitHubVersionDisplay.Tests
+{
+    public class VersionTests
+    {
+        [Fact]
+        public void Version_Should_Contain_GitHash()
+        {
+            var version = Assembly.GetAssembly(typeof(Program))
+                ?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                ?.InformationalVersion;
+
+            Assert.NotNull(version);
+            Assert.Matches(@"^1\.0\.0\+[0-9a-f]{7,40}$", version!); // GitのSHA-1
+        }
+    }
+}
