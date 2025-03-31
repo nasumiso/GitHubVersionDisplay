3b13d7b464692d3c3d87fe51dffb91dcc7bb5b4b
K.Sohara <srqys795@yahoo.co.jp>
2025-03-31 15:31:27 +0900

    C#プロジェクトのバージョン情報を表示するプログラムを追加
    AssemblyInformationalVersion を使って Git コミットハッシュ付きで表示。

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+class Program
+{
+    /// <summary>
+    ///C#プロジェクトのバージョンを表示する
+    /// 例：アセンブリバージョン+Gitバージョン
+    /// Version: 1.0.0+014129cfcdc87493909bd12bd2b7383d086439aa
+    /// </summary>
+    static void Main()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        Console.WriteLine($"Version: {version}");
+    }
+}
